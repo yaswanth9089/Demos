c0d1a430069f1dc7dd8dc6b255266c6a75012ce3
reddyyaswanth <reddyyaswanth9089@gmail.com>
2021-12-02 11:39:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Hello Dinesh!!");
+        }
+    }
+}
